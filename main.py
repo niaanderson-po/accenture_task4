aba062d39973b7694dd0a2adbd51cb075e9ee978
niaanderson-po <niaanderson.po@gmail.com>
2024-10-14 23:54:01 -0700

    Acceture Task 4: starter files

---
+++ main.py
@@ -0,0 +1,26 @@
+import re 
+
+# Question 1: -----------------------------------
+regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$'
+      
+def check_email(email):  
+    pass
+      
+def check_psw_upper(psw):
+    pass
+
+def check_psw_digit(psw):
+    pass
+
+def check_psw_lower(psw):
+    pass
+
+def check_psw(psw):
+    pass    
+
+# New functions: ------------------------------------
+def check_psw_equal(psw1, psw2):
+    pass
+
+def check_credentials(email, psw1, psw2):
+    pass
---
+++ simple_unit_test.py
@@ -0,0 +1,8 @@
+def hello_function():
+    return "hello"
+
+def test_hello_funtion():
+    string = "hello"
+    assert (string == hello_function()) is True
+
+test_hello_funtion()
